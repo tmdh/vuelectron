b43db5df32e7b94d7cefca0ab0abea2426ee9799
tmdh <tarequemd.hanif@yahoo.com>
2021-01-07 16:15:39 +0300

    load vue css img with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,64 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = [
   {
     entry: {
-      main: "./src/main.js",
+      main: "./src/main.js"
     },
     output: {
       path: path.resolve(__dirname, "dist"),
-      filename: "[name].js",
+      filename: "[name].js"
     },
-    target: "electron-main",
+    target: "electron-main"
   },
   {
     entry: {
-      index: "./src/renderer/index.js",
+      index: "./src/renderer/index.js"
     },
     output: {
       path: path.resolve(__dirname, "dist/app"),
-      filename: "[name].js",
+      filename: "[name].js"
     },
     target: "electron-renderer",
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader"
+        },
+        {
+          test: /\.css$/,
+          use: [
+            process.env.NODE_ENV !== "production"
+              ? "vue-style-loader"
+              : MiniCssExtractPlugin.loader,
+            "css-loader"
+          ]
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                esModule: false
+              }
+            }
+          ]
+        }
+      ]
+    },
     plugins: [
       new HtmlWebpackPlugin({
-        template: "./src/renderer/index.html",
+        template: "./src/renderer/index.html"
       }),
-    ],
-  },
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "style.css"
+      })
+    ]
+  }
 ];
